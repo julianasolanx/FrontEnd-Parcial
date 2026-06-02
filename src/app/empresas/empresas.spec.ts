3c282f34e07982851458f6eb633c540251b182d2
julianasolanx <julianansolanoo@gmail.com>
2026-06-02 18:53:46 -0500

    pagina inicial

---
+++ src/app/empresas/empresas.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Empresas } from './empresas';
+
+describe('Empresas', () => {
+  let component: Empresas;
+  let fixture: ComponentFixture<Empresas>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Empresas],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Empresas);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/empresas/empresas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empresas',
+  imports: [],
+  templateUrl: './empresas.html',
+  styleUrl: './empresas.css',
+})
+export class Empresas {}
